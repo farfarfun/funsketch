8297031a47f4c90f491a1c3f9d682ea60eced8e7
farfun <1007530194@qq.com>
2024-12-20 11:23:44 +0800

    add

--- src/funsketch/sketch/task/__init__.py
+++ src/funsketch/sketch/task/__init__.py
@@ -1,4 +1,6 @@
-from .base import BaseTask
+from .audio import AudioTask
+from .base import BaseTask, TaskRun
 from .load import LoadTask
+from .text import TextTask
 
-__all__ = ["BaseTask", "LoadTask"]
+__all__ = ["BaseTask", "LoadTask", "AudioTask", "TextTask", "TaskRun"]
