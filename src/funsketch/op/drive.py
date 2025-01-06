49d015b366c1f2b1e7a102467ab50857e1884b69
farfun <1007530194@qq.com>
2025-01-06 23:13:16 +0800

    add

--- src/funsketch/op/drive.py
+++ src/funsketch/op/drive.py
@@ -1,8 +1,10 @@
 from fundrive.drives.alipan import AlipanDrive
 from fundrive.drives.baidu import BaiDuDrive
+from fundrive.drives.webdav import WebDavDrive
+from funsecret import read_secret
 
 
-def get_default_drive():
+def get_default_drive3():
     driver = BaiDuDrive()
     driver.login()
     return driver
@@ -11,4 +13,22 @@ def get_default_drive():
 def get_default_drive2():
     driver = AlipanDrive()
     driver.login(is_resource=True)
+    return driver
+
+
+def get_default_drive():
+    driver = WebDavDrive()
+    driver.login(
+        server_url=read_secret("funsketch", "webdav", "server_url"),
+        username=read_secret(
+            "funsketch",
+            "webdav",
+            "username",
+        ),
+        password=read_secret(
+            "funsketch",
+            "webdav",
+            "password",
+        ),
+    )
     return driver
