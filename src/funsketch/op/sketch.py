5a56b58fcb87ea1ae856e6ff278ff4a4bbda785a
farfun <1007530194@qq.com>
2025-01-07 20:36:46 +0800

    add

--- src/funsketch/op/sketch.py
+++ src/funsketch/op/sketch.py
@@ -12,8 +12,8 @@ logger = getLogger("funsketch")
 
 def sync_sketch_data(
     driver: BaseDrive = None,
-    sketch_fid="/funsketch-短剧",
-    funsketch_fid="/funsketch",
+    sketch_fid="/farfarfun/funsketch-短剧",
+    funsketch_fid="/farfarfun/funsketch",
 ):
     driver = driver or get_default_drive()
 
